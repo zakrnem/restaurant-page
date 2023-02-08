c35284810af1c03095874c52a1aa0e10a9055d78
zakrnem <zakrnem@zohomail.com>
2023-02-07 18:54:10 -0600

    Replace SVG icons for contact menu

--- src/contact.js
+++ src/contact.js
@@ -1,13 +1,16 @@
+import { footer } from './footer.js';
+
 export function contactDOM() {
     let main = document.createElement('main')
     let container = document.createElement('div')
     container.className = 'contact'
+   /*  let contactInfo */
         
         let phone = document.createElement('div')
         container.appendChild(phone)
             let phoneLogo = document.createElement('img')
                 phoneLogo.className = 'contact-icon'
-                phoneLogo.src = './images/location-svg-repo-com.svg'
+                phoneLogo.src = './images/phone-svgrepo-com.svg'
                 phone.appendChild(phoneLogo)
 
             let phoneNumber = document.createElement('p')
@@ -18,14 +21,15 @@ export function contactDOM() {
         container.appendChild(socialMedia)
             let socialLogo = document.createElement('img')
                 socialLogo.className = 'contact-icon'
-                socialLogo.src = './images/location-svg-repo-com.svg'
+                socialLogo.src = './images/telegram-svgrepo-com.svg'
                 socialMedia.appendChild(socialLogo)
 
             let socialUser = document.createElement('p')
                 socialUser.textContent = 'shawarma.paradise'
-                phone.appendChild(socialUser)
+                socialMedia.appendChild(socialUser)
 
     main.appendChild(container)
     let content = document.querySelector('#content')
     content.appendChild(main)
+    footer()
 }
