0cbd26550bd4d457fff858488674afbf0b486cfc
zakrnem <zakrnem@zohomail.com>
2023-02-07 21:01:07 -0600

    Add gmap canvas to contact menu

--- src/contact.js
+++ src/contact.js
@@ -4,10 +4,12 @@ export function contactDOM() {
     let main = document.createElement('main')
     let container = document.createElement('div')
     container.className = 'contact'
-   /*  let contactInfo */
+    let contactInfo = document.createElement('div')
+    contactInfo.className = 'info'
         
         let phone = document.createElement('div')
-        container.appendChild(phone)
+        phone.className = 'phone'
+        contactInfo.appendChild(phone)
             let phoneLogo = document.createElement('img')
                 phoneLogo.className = 'contact-icon'
                 phoneLogo.src = './images/phone-svgrepo-com.svg'
@@ -18,7 +20,8 @@ export function contactDOM() {
                 phone.appendChild(phoneNumber)
 
         let socialMedia = document.createElement('div')
-        container.appendChild(socialMedia)
+        socialMedia.className = 'social'
+        contactInfo.appendChild(socialMedia)
             let socialLogo = document.createElement('img')
                 socialLogo.className = 'contact-icon'
                 socialLogo.src = './images/telegram-svgrepo-com.svg'
@@ -28,8 +31,17 @@ export function contactDOM() {
                 socialUser.textContent = 'shawarma.paradise'
                 socialMedia.appendChild(socialUser)
 
+    container.appendChild(mapHTML())
+    container.appendChild(contactInfo)
     main.appendChild(container)
     let content = document.querySelector('#content')
     content.appendChild(main)
     footer()
+}
+
+function mapHTML() {
+    let mapEmbed = document.createElement('div')
+    mapEmbed.innerHTML = 
+    '<div class="gmap_canvas"><iframe width="350" height="250" id="gmap_canvas" src="https://maps.google.com/maps?q=1%20Galle%20Face%20Shangri-La%20Hotel,%20Colombo%2000200%20Sri%20Lanka&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div>'
+    return mapEmbed
 }
